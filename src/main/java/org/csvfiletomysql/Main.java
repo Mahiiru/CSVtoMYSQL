2dc5be49844c4651aa5b63fbda439bf3ba58dc59
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-02-05 18:08:15 -0300

    readCSvFile method done

--- src/main/java/org/csvfiletomysql/Main.java
+++ src/main/java/org/csvfiletomysql/Main.java
@@ -7,29 +7,46 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/movies";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/imdb_top_1000_movies";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "password";
+    private static final String DB_PASSWORD = "";
 
     public static void main(String[] args) {
-        String fileName = "movies.csv";
+
+        String fileName = "src/main/java/org/csvfiletomysql/imdb_top_1000.csv";
 
         List<Movie> movies = readCsvFile(fileName);
+
 
         try {
             Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
-            String sql = "INSERT INTO movies (title, year, rating) VALUES (?, ?, ?)";
+            String sql = "INSERT INTO `movies`(`movie_poster_link`, `movie_series_title`, `movie_released_year`, `movie_certificate`, `movie_runtime`, `movie_genre`, `movie_imdb_rating`, `movie_overview`, `movie_meta_score`, `movie_director`, `movie_star1`, `movie_star2`, `movie_star3`, `movie_star4`, `movie_no_of_votes`, `movie_gross`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             PreparedStatement statement = conn.prepareStatement(sql);
 
             for (Movie movie : movies) {
-                statement.setString(1, movie.getTitle());
-                statement.setInt(2, movie.getYear());
-                statement.setDouble(3, movie.getRating());
+                statement.setString(1, movie.getPoster_link());
+                statement.setString(2, movie.getSeries_title());
+                statement.setInt(3, movie.getReleased_year());
+                statement.setString(4,movie.getCertificate());
+                statement.setString(5,movie.getRuntime());
+                statement.setString(6,movie.getGenre());
+                statement.setFloat(7,movie.getImdb_rating());
+                statement.setString(8,movie.getOverview());
+                statement.setInt(9,movie.getMeta_score());
+                statement.setString(10,movie.getDirector());
+                statement.setString(11,movie.getStar1());
+                statement.setString(12,movie.getStar2());
+                statement.setString(13,movie.getStar3());
+                statement.setString(14,movie.getStar4());
+                statement.setLong(15,movie.getNo_of_votes());
+                statement.setString(16,movie.getGross());
 
                 statement.executeUpdate();
             }
@@ -38,6 +55,7 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 
     private static List<Movie> readCsvFile(String fileName) {
@@ -49,14 +67,32 @@ public class Main {
             String line = reader.readLine(); // skip the header line
 
             while ((line = reader.readLine()) != null) {
-                String[] columns = line.split(",");
-
-                Movie movie = new Movie();
-                movie.setTitle(columns[0]);
-                movie.setYear(Integer.parseInt(columns[1]));
-                movie.setRating(Double.parseDouble(columns[2]));
-
-                movies.add(movie);
+                String[] columns = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                List<String> evalColumns = List.of(columns);
+                if(columns.length >= 16 && !evalColumns.contains("")){
+                    try {
+                        Movie movie = new Movie();
+                        movie.setPoster_link(columns[0]);
+                        movie.setSeries_title(columns[1]);
+                        movie.setReleased_year(Integer.parseInt(columns[2]));
+                        movie.setCertificate(columns[3]);
+                        movie.setRuntime(columns[4]);
+                        movie.setGenre(columns[5]);
+                        movie.setImdb_rating(Float.parseFloat(columns[6]));
+                        movie.setOverview(columns[7]);
+                        movie.setMeta_score(Integer.parseInt(columns[8]));
+                        movie.setDirector(columns[9]);
+                        movie.setStar1(columns[10]);
+                        movie.setStar2(columns[11]);
+                        movie.setStar3(columns[12]);
+                        movie.setStar4(columns[13]);
+                        movie.setNo_of_votes(Long.parseLong(columns[14]));
+                        movie.setGross(columns[15]);
+                        movies.add(movie);
+                    } catch (NumberFormatException e) {
+                        System.out.println("");
+                    }
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/org/csvfiletomysql/Movie.java
+++ src/main/java/org/csvfiletomysql/Movie.java
@@ -2,31 +2,170 @@ package org.csvfiletomysql;
 
 
 public class Movie {
-    private String title;
-    private int year;
-    private double rating;
+    private String poster_link;
+    private String series_title;
+    private int released_year;
+    private String certificate;
+    private String runtime;
+    private String genre;
+    private float imdb_rating;
+    private String overview;
+    private int meta_score;
+    private String director;
+    private String star1;
+    private String star2;
+    private String star3;
+    private String star4;
+    private long no_of_votes;
+    private String gross;
 
-    public String getTitle() {
-        return title;
+    public String getPoster_link() {
+        return poster_link;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setPoster_link(String poster_link) {
+        this.poster_link = poster_link;
     }
 
-    public int getYear() {
-        return year;
+    public String getSeries_title() {
+        return series_title;
     }
 
-    public void setYear(int year) {
-        this.year = year;
+    public void setSeries_title(String series_title) {
+        this.series_title = series_title;
     }
 
-    public double getRating() {
-        return rating;
+    public int getReleased_year() {
+        return released_year;
     }
 
-    public void setRating(double rating) {
-        this.rating = rating;
+    public void setReleased_year(int released_year) {
+        this.released_year = released_year;
+    }
+
+    public String getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public float getImdb_rating() {
+        return imdb_rating;
+    }
+
+    public void setImdb_rating(float imdb_rating) {
+        this.imdb_rating = imdb_rating;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public int getMeta_score() {
+        return meta_score;
+    }
+
+    public void setMeta_score(int meta_score) {
+        this.meta_score = meta_score;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getStar1() {
+        return star1;
+    }
+
+    public void setStar1(String star1) {
+        this.star1 = star1;
+    }
+
+    public String getStar2() {
+        return star2;
+    }
+
+    public void setStar2(String star2) {
+        this.star2 = star2;
+    }
+
+    public String getStar3() {
+        return star3;
+    }
+
+    public void setStar3(String star3) {
+        this.star3 = star3;
+    }
+
+    public String getStar4() {
+        return star4;
+    }
+
+    public void setStar4(String star4) {
+        this.star4 = star4;
+    }
+
+    public long getNo_of_votes() {
+        return no_of_votes;
+    }
+
+    public void setNo_of_votes(long no_of_votes) {
+        this.no_of_votes = no_of_votes;
+    }
+
+    public String getGross() {
+        return gross;
+    }
+
+    public void setGross(String gross) {
+        this.gross = gross;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "poster_link='" + poster_link + '\'' +
+                ", series_title='" + series_title + '\'' +
+                ", released_year=" + released_year +
+                ", certificate='" + certificate + '\'' +
+                ", runtime='" + runtime + '\'' +
+                ", genre='" + genre + '\'' +
+                ", imdb_rating=" + imdb_rating +
+                ", overview='" + overview + '\'' +
+                ", meta_score=" + meta_score +
+                ", director='" + director + '\'' +
+                ", star1='" + star1 + '\'' +
+                ", star2='" + star2 + '\'' +
+                ", star3='" + star3 + '\'' +
+                ", star4='" + star4 + '\'' +
+                ", no_of_votes=" + no_of_votes +
+                ", gross='" + gross + '\'' +
+                '}';
     }
 }
